9fcdceddd6ef4ae6fb1464c00d8dde1ea737f9df
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-10-12 22:41:40 -0400

    feat: implementation data validation

--- src/domain/entities/BaseEntity.ts
+++ src/domain/entities/BaseEntity.ts
@@ -1,7 +1,12 @@
+import { IsDate, IsUUID } from 'class-validator';
 import { v4 as uuid } from 'uuid';
 
 export class BaseEntity {
+
+  @IsUUID()
   id?: string;
+
+  @IsDate()
   createdAt?: Date;
 
   constructor() {
